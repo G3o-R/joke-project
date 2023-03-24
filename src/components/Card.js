007969e58f85661d36c8f9a4ff94ac6be2965eb1
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-03-24 10:56:20 -0700

    added styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "../styles/Card.scss"
 
 function Card(){
     const [formData,setFrom] = useState({
@@ -19,9 +20,10 @@ function Card(){
     }
 
     return(<div className="Card">
+        <h2>Add your name and age!!!</h2>
         <form onSubmit={handleSubmit}>
-        <input type="text" name="age" value={age} placeholder="age..." onChange={handleChange}/>
-        <input type="text" name="name" value={name} placeholder="name..." onChange={handleChange}/>
+        age:<input type="text" name="age" value={age} placeholder="age..." onChange={handleChange}/>
+        name:<input type="text" name="name" value={name} placeholder="name..." onChange={handleChange}/>
         <button type="submit">Enter</button>
         </form>
         <h2 className="output">Your name is {age} and you are {name} years old!!!</h2>
